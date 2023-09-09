fbcd781c235c1dedffa56bf5111521c549611e20
Lucas Anjos <oargus.g@gmail.com>
2023-09-09 10:54:47 -0300

    Meu arquivo Hello World

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+nome = "Lucas"
+profissao = "Professor e programador"
+
+print(f"Este é o primeiro programa de {nome}")
+print("Eu sou", profissao, "e decidi trilhar os caminhos do python")
+
+resposta = input("Você está pronto para vir comigo (S/N)? ")
