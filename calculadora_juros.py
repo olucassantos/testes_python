e731e02d235a0a7f4ee80200d93c17b1d482f59d
Lucas Anjos <oargus.g@gmail.com>
2023-09-09 15:49:06 -0300

    Adicionado limpeza de tela

--- calculadora_juros.py
+++ calculadora_juros.py
@@ -1,4 +1,5 @@
 # Programa para calcular o juros
+import os
 
 print("============================================")
 print("============== JUROS PRO 1000 ==============")
@@ -9,5 +10,7 @@ taxa_anual = float(input("Digite a taxa anual em porcentagem: "))
 tempo_anos = float(input("Digite o tempo em anos: "))
 
 montante = valor_inicial + (valor_inicial * (taxa_anual / 100) * tempo_anos)
+
+os.system('cls' if os.name == 'nt' else 'clear')
 
 print(f"O valor total será de: {montante}")
