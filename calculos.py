cb4874048a6bcc054be2f64242d937eab0ba99c6
Lucas Anjos <oargus.g@gmail.com>
2023-09-09 12:08:19 -0300

    Arquivo de estudos sobre os calculos

---
+++ calculos.py
@@ -0,0 +1,9 @@
+# Arquivo relacionado a testes de cálculos
+numero1 = 12
+numero2 = 13
+
+print(numero1 + numero2)
+
+resultado = numero1 + numero2
+print("O resultado é:", resultado)
+print(f"O resultado é: {resultado}")
